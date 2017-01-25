c5ed7ae163a1011c3945c4b6695935270ed016af
haklabrador <haklabrador@gmail.com>
2017-01-25 21:46:46 +0100

    ekvivalentan odt file parser u pythonu

---
+++ odt.py
@@ -0,0 +1,28 @@
+import zipfile
+import xml.etree.ElementTree as ET
+
+def open(filename):
+    return zipfile.ZipFile(filename)
+
+def get_text(odtfile):
+    # procitaj sadrzaj content.xml iz zip fajla
+    contents = odtfile.read('content.xml')
+
+    # izparsiraj XML
+    root = ET.fromstring(contents)
+
+    # izuci textualni sadrzaj iz text:p elemenata
+    text = ''
+
+    for el1 in root.findall('{urn:oasis:names:tc:opendocument:xmlns:office:1.0}body'):
+        for el2 in el1.findall('{urn:oasis:names:tc:opendocument:xmlns:office:1.0}text'):
+            for el3 in el2.findall('{urn:oasis:names:tc:opendocument:xmlns:text:1.0}p'):
+                #print 'el3:', el3.text
+                if el3.text:
+                    text += el3.text
+
+    return text
+
+if __name__ == '__main__':
+    print get_text(open('doc1.odt')) # ispisi textualni sadrzaj odt fajla
+    
